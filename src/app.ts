eb24a99796b64dcfc5a101c87a2eb259cb373b2c
Alex Neamtu <alexneamtu@gmail.com>
2022-08-12 13:19:34 +0300

    Initial

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import express from 'express';
+import { mdToPdf } from 'md-to-pdf';
+import { extname, parse } from 'path';
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  const url = req.query.url as string;
+  const filename = req.query.filename as string;
+
+  if (!url) {
+    return res.status(500).send('Missing url.');
+  }
+
+  try {
+    new URL(url);
+  } catch (e) {
+    return res.status(500).send('Invalid url.');
+  }
+
+  if (extname(url) !== '.md') {
+    return res.status(500).send('Invalid file type.');
+  }
+
+  const { data } = await axios.get(url, { timeout: 1000 });
+  const pdf = await mdToPdf({ content: data });
+  res.setHeader('Content-Disposition', `attachment; filename=${filename || 'output'}.pdf`);
+
+  return res.end(pdf.content);
+});
+
+app.listen(80, () => {
+  console.info(`server started at http://localhost:${80}`);
+});
